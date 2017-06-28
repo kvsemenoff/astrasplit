ec542dc767a5c1cf19f8e58acc0bebccc53e827d
roshett <roshette@yandex.ru>
2017-06-28 16:12:17 +0300

    some

--- includes/breadcrumbs.php
+++ includes/breadcrumbs.php
@@ -5,3 +5,15 @@
  * Date: 27.06.2017
  * Time: 20:27
  */
+?>
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="breadcrumbs">
+				<ul>
+					<li><a href="#">Breadcrumb</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
