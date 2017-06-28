46b3da8b09fc1ba65ae3799296f92078c9bed3a9
roshett <roshette@yandex.ru>
2017-06-28 16:54:08 +0300

    some

--- actions.php
+++ actions.php
@@ -1,7 +1,39 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 27.06.2017
- * Time: 19:43
- */
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Акции</title>
+	<?php require_once('includes/header.php'); ?>
+<body>
+	<div class="wrapper">
+		<div class="content">
+            <?php require_once('includes/header-top.php'); ?>
+            <?php require_once('includes/breadcrumbs.php'); ?>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h1 class="actions__h1">Акции</h1>
+					</div>
+					<div class="clearfix"></div>
+					<div class="col-md-12">
+						<div class="act__block">
+							<img src="img/act1.png" alt="">
+							<span class="date">20.06.2017</span><br>
+							<span class="topic"></span><br>
+							<div class="button">
+								Тест
+							</div>
+						</div>
+					</div>
+					<div class="col-md-12"></div>
+					<div class="col-md-12"></div>
+				</div>
+			</div>
+		</div>
+		<div class="footer">
+			<?php require_once('includes/footer.php'); ?> 
+		</div>
+	</div>
+</body>
+</html>
