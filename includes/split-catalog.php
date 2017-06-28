7d5d95d3f7be45ea122e7d22f7d6acfd3cc744f9
Kirill Solovyev <Kirill.Solovyev.98@gmail.com>
2017-06-28 17:47:54 +0600

    split-catalog

--- includes/split-catalog.php
+++ includes/split-catalog.php
@@ -1,7 +1,116 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 27.06.2017
- * Time: 20:25
- */
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="../css/fonts.css">
+<link rel="stylesheet" href="../css/reset.css">
+<link rel="stylesheet" href="../block/h1/style.css">
+<link rel="stylesheet" href="../block/h2/style.css">
+<link rel="stylesheet" href="../block/h3/style.css">
+<link href="https://fonts.googleapis.com/css?family=Arsenal:400,700|Noto+Sans" rel="stylesheet">
+<link rel="stylesheet" href="../css/split-catalog-style.css">
+</head>
+<body>
+	<div class='split-catalog'>
+		<div class='split-catalog-left'>
+			<h1>Каталог сплит-систем и кондиционеров</h1>
+			<div class='split-catalog-item'>
+				<img src='../img/aux_logo-min.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/ballu-min.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/Carrier-min.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/daikin-min.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/dantex-logo.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/electrolux-min.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/Fujitsu-min.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/general.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/hisense_logo.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/hitachi_logo.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/kentatsu_logo.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/lessar-min.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/lg-min.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/mdv.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/MH_logo.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/midea-min.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/gree.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/Haier-min.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/Mitsubishi_electric-min.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/neoclima_logo.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/Panasonic-min.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/pioneer.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/QC-logo.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/rovex_logo.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/royal-clima.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/Samsung-min.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/Toshiba-min.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/tosot-min-min.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/Zan_logo.jpg' />
+			</div>
+			<div class='split-catalog-item'>
+				<img src='../img/Zerten.jpg' />
+			</div>
+		</div>
+		<div class='split-catalog-right'>
+			<div class='split-catalog-sidebar1'>
+				<h2>ЧИСТКА РЕМОНТ заправка</h2>
+				<h3>СПЛИТ СИСТЕМ  КОНДИЦИОНЕРОВ</h3>
+			</div>
+			<div class='split-catalog-sidebar2'>
+				<h3>БЫСТРАЯ  ДОСТАВКА  </br> И УСТАНОВКА</h3>
+			</div>
+		</div>
+	</div>
+</body>
