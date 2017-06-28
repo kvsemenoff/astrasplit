c33665ccb6339cf3c2ac57cbece277eeb0e09170
roshett <roshette@yandex.ru>
2017-06-28 12:04:58 +0300

    body

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,40 @@
-
+<div class="wrap-footer-di">
+	<div class="container">
+		<div class="row">
+			<div class="wrap-footer-content-di">
+				<div class="col-md-3">
+					<span>Информация</span>
+					<div class="wrap-ul-di">
+						<ul>
+						<li><a href="">Сплит системы</a></li>
+						<li><a href="">Полупромышленный сплит</a></li>
+						<li><a href="">Сервисный центр</a></li>
+						<li><a href="">Организациям</a></li>
+						<li><a href="">Доставка и оплата</a></li>
+						<li><a href="">Цены</a></li>
+					</ul>
+					</div>
+				</div>
+				<div class="col-md-2">
+					<div class="wrap-ul-di two-di">
+						<ul>
+						<li><a href="">О компании</a></li>
+						<li><a href="">Акции</a></li>
+						<li><a href="">Новости</a></li>
+						<li><a href="">Статьи</a></li>
+						<li><a href="">Отзывы</a></li>
+						<li><a href="">Контакты</a></li>
+					</ul>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<span>Контакты</span>
+				</div>
+				<div class="col-md-4"></div>
+			</div>
+		</div>
+	</div>
+</div>
 
 
 <?php require_once('popup.php'); ?>
