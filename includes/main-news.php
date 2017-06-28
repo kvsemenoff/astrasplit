72437f676680b0dda4918aaed31e7493cd025970
boka <web_masters_07@mail.ru>
2017-06-28 14:31:14 +0300

    news

--- includes/main-news.php
+++ includes/main-news.php
@@ -1,7 +1,29 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 27.06.2017
- * Time: 20:26
- */
+<div class="main-news">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Новости</h1><a href="#" class="all_news"><span>все новости &#187;</span></a>
+			</div>
+			<div class="clearfix"></div>
+			<div class="col-md-4">
+				<p>
+					<span>05.10.16</span><br>
+					<a href="#">Рыбные тексты также применяются для демонстрирования различных видов шрифта</a> 
+				</p>
+			</div>
+			<div class="col-md-4">
+				<p>
+					<span>05.10.16</span><br>
+					<a href="#">Рыбные тексты также применяются для демонстрирования различных видов шрифта и в разработке макетов</a> 
+				</p>
+			</div>
+			<div class="col-md-4">
+				<p>
+					<span>05.10.16</span><br>
+					<a href="#">Рыбные тексты также применяются для демонстрирования различных видов</a> 
+				</p>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+</div>
