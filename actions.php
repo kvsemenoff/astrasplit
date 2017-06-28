ed056bede4422cf796c1445e10f2796113569167
roshett <roshette@yandex.ru>
2017-06-28 17:15:04 +0300

    some

--- actions.php
+++ actions.php
@@ -5,35 +5,60 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Акции</title>
 	<?php require_once('includes/header.php'); ?>
-<body>
-	<div class="wrapper">
-		<div class="content">
-            <?php require_once('includes/header-top.php'); ?>
-            <?php require_once('includes/breadcrumbs.php'); ?>
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12">
-						<h1 class="actions__h1">Акции</h1>
-					</div>
-					<div class="clearfix"></div>
-					<div class="col-md-12">
+	<body>
+		<div class="wrapper">
+			<div class="content">
+				<?php require_once('includes/header-top.php'); ?>
+				<?php require_once('includes/breadcrumbs.php'); ?>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<h1 class="actions__h1">Акции</h1>
+						</div>
+						<div class="clearfix"></div>
+						<div class="somePad"></div>
 						<div class="act__block">
-							<img src="img/act1.png" alt="">
-							<span class="date">20.06.2017</span><br>
-							<span class="topic"></span><br>
-							<div class="button">
-								Тест
+							<div class="col-md-3">
+								<img src="img/act1.png" alt="">
+							</div>
+							<div class="col-md-9">
+
+								<span class="date-di">20.06.2017</span><br>
+								<span class="topic-di">Мы предоставляем скидку всем покупателям</span><br>
+								<a href='#' class='button button_tranparent'>подробнее</a>
 							</div>
 						</div>
+						<div class="clearfix"></div>
+						<div class="act__block">
+							<div class="col-md-3">
+								<img src="img/act2.png" alt="">
+							</div>
+							<div class="col-md-9">
+
+								<span class="date-di">20.06.2017</span><br>
+								<span class="topic-di">Только до 20 июля у нас действует специальное предложение</span><br>
+								<a href='#' class='button button_tranparent'>подробнее</a>
+							</div>
+						</div>
+						<div class="clearfix"></div>
+						<div class="act__block">
+							<div class="col-md-3">
+								<img src="img/act3.png" alt="">
+							</div>
+							<div class="col-md-9">
+								<span class="date-di">20.06.2017</span><br>
+								<span class="topic-di">Специальное предложение по сплит-системам</span><br>
+								<a href='#' class='button button_tranparent'>подробнее</a>
+							</div>
+						</div>
+						<div class="clearfix"></div>
+						<div class="somePad2"></div>
 					</div>
-					<div class="col-md-12"></div>
-					<div class="col-md-12"></div>
 				</div>
 			</div>
+			<div class="footer">
+				<?php require_once('includes/footer.php'); ?> 
+			</div>
 		</div>
-		<div class="footer">
-			<?php require_once('includes/footer.php'); ?> 
-		</div>
-	</div>
-</body>
+	</body>
 </html>
