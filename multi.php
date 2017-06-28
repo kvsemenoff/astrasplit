e6bd6f53efcb4dbd96ada818809da836fd83a98b
boka <web_masters_07@mail.ru>
2017-06-28 16:51:07 +0300

    sdds

--- multi.php
+++ multi.php
@@ -1,7 +1,20 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 27.06.2017
- * Time: 19:38
- */
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Главная</title>
+	<?php require_once('includes/header.php'); ?>
+<body>
+	<div class="wrapper">
+		<div class="content">
+            <?php require_once('includes/header-top.php'); ?>
+            <?php require_once('includes/breadcrumbs.php'); ?>
+	
+		</div>
+		<div class="footer">
+			<?php require_once('includes/footer.php'); ?> 
+		</div>
+	</div>
+</body>
+</html>
