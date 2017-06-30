280e3ed9b52cd50536bade388d038462cf52034c
Kirill Solovyev <Kirill.Solovyev.98@gmail.com>
2017-06-30 13:25:10 +0600

    q

--- includes/split-catalog.php
+++ includes/split-catalog.php
@@ -3,94 +3,94 @@
 		<div class='split-catalog-left'>
 			<h1>Каталог сплит-систем и кондиционеров</h1>
 			<div class='split-catalog-item'>
-				<img src='../img/aux_logo-min.jpg' />
+				<img src='../img/aux_logo-min.jpg' alt='Картинка'/>
 			</div>
 			<div class='split-catalog-item'>
-				<img src='../img/ballu-min.jpg' />
+				<img src='../img/ballu-min.jpg' alt='Картинка'/>
 			</div>
 			<div class='split-catalog-item'>
-				<img src='../img/Carrier-min.jpg' />
+				<img src='../img/Carrier-min.jpg' alt='Картинка' />
 			</div>
 			<div class='split-catalog-item'>
-				<img src='../img/daikin-min.jpg' />
+				<img src='../img/daikin-min.jpg' alt='Картинка' />
 			</div>
 			<div class='split-catalog-item'>
-				<img src='../img/dantex-logo.jpg' />
+				<img src='../img/dantex-logo.jpg' alt='Картинка' />
 			</div>
 			<div class='split-catalog-item'>
-				<img src='../img/electrolux-min.jpg' />
+				<img src='../img/electrolux-min.jpg' alt='Картинка' />
 			</div>
 			<div class='split-catalog-item'>
-				<img src='../img/Fujitsu-min.jpg' />
+				<img src='../img/Fujitsu-min.jpg'  alt='Картинка' />
 			</div>
 			<div class='split-catalog-item'>
-				<img src='../img/general.jpg' />
+				<img src='../img/general.jpg' alt='Картинка' />
 			</div>
 			<div class='split-catalog-item'>
-				<img src='../img/hisense_logo.jpg' />
+				<img src='../img/hisense_logo.jpg' alt='Картинка' />
 			</div>
 			<div class='split-catalog-item'>
-				<img src='../img/hitachi_logo.jpg' />
+				<img src='../img/hitachi_logo.jpg' alt='Картинка' />
 			</div>
 			<div class='split-catalog-item'>
-				<img src='../img/kentatsu_logo.jpg' />
+				<img src='../img/kentatsu_logo.jpg'alt='Картинка'  />
 			</div>
 			<div class='split-catalog-item'>
-				<img src='../img/lessar-min.jpg' />
+				<img src='../img/lessar-min.jpg' alt='Картинка' />
 			</div>
 			<div class='split-catalog-item'>
-				<img src='../img/lg-min.jpg' />
+				<img src='../img/lg-min.jpg' alt='Картинка' />
 			</div>
 			<div class='split-catalog-item'>
 				<img src='../img/mdv.jpg' />
 			</div>
 			<div class='split-catalog-item'>
-				<img src='../img/MH_logo.jpg' />
+				<img src='../img/MH_logo.jpg' alt='Картинка' />
 			</div>
 			<div class='split-catalog-item'>
-				<img src='../img/midea-min.jpg' />
+				<img src='../img/midea-min.jpg' alt='Картинка' />
 			</div>
 			<div class='split-catalog-item'>
 				<img src='../img/gree.jpg' />
 			</div>
 			<div class='split-catalog-item'>
-				<img src='../img/Haier-min.jpg' />
+				<img src='../img/Haier-min.jpg' alt='Картинка'  />
 			</div>
 			<div class='split-catalog-item'>
-				<img src='../img/Mitsubishi_electric-min.jpg' />
+				<img src='../img/Mitsubishi_electric-min.jpg' alt='Картинка' />
 			</div>
 			<div class='split-catalog-item'>
-				<img src='../img/neoclima_logo.jpg' />
+				<img src='../img/neoclima_logo.jpg' alt='Картинка' />
 			</div>
 			<div class='split-catalog-item'>
-				<img src='../img/Panasonic-min.jpg' />
+				<img src='../img/Panasonic-min.jpg' alt='Картинка' />
 			</div>
 			<div class='split-catalog-item'>
-				<img src='../img/pioneer.jpg' />
+				<img src='../img/pioneer.jpg' alt='Картинка' />
 			</div>
 			<div class='split-catalog-item'>
-				<img src='../img/QC-logo.jpg' />
+				<img src='../img/QC-logo.jpg' alt='Картинка' />
 			</div>
 			<div class='split-catalog-item'>
-				<img src='../img/rovex_logo.jpg' />
+				<img src='../img/rovex_logo.jpg' alt='Картинка'  />
 			</div>
 			<div class='split-catalog-item'>
-				<img src='../img/royal-clima.jpg' />
+				<img src='../img/royal-clima.jpg' alt='Картинка' />
 			</div>
 			<div class='split-catalog-item'>
-				<img src='../img/Samsung-min.jpg' />
+				<img src='../img/Samsung-min.jpg' alt='Картинка' />
 			</div>
 			<div class='split-catalog-item'>
-				<img src='../img/Toshiba-min.jpg' />
+				<img src='../img/Toshiba-min.jpg' alt='Картинка' />
 			</div>
 			<div class='split-catalog-item'>
-				<img src='../img/tosot-min-min.jpg' />
+				<img src='../img/tosot-min-min.jpg'  alt='Картинка'/>
 			</div>
 			<div class='split-catalog-item'>
-				<img src='../img/Zan_logo.jpg' />
+				<img src='../img/Zan_logo.jpg' alt='Картинка' />
 			</div>
 			<div class='split-catalog-item'>
-				<img src='../img/Zerten.jpg' />
+				<img src='../img/Zerten.jpg' alt='Картинка' />
 			</div>
 		</div>
 		<div class='split-catalog-right'>
@@ -99,7 +99,7 @@
 				<h3>СПЛИТ СИСТЕМ  КОНДИЦИОНЕРОВ</h3>
 			</div>
 			<div class='split-catalog-sidebar2'>
-				<h3>БЫСТРАЯ  ДОСТАВКА  </br> И УСТАНОВКА</h3>
+				<h3>БЫСТРАЯ  ДОСТАВКА  <br /> И УСТАНОВКА</h3>
 			</div>
 		</div>
 	</div>
