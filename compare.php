e082ae919e89129bdee3060606486625318c7e38
roshett <roshette@yandex.ru>
2017-06-30 12:13:13 +0300

    someFix

--- compare.php
+++ compare.php
@@ -1,7 +1,156 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 27.06.2017
- * Time: 19:40
- */
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Главная</title>
+	<?php require_once('includes/header.php'); ?>
+<body>
+	<div class="wrapper">
+		<div class="content">
+            <?php require_once('includes/header-top.php'); ?>
+            <?php require_once('includes/breadcrumbs.php'); ?>
+            <div class="container">
+            	<div class="row">
+            	<!--	<div class="col-md-12 col-sm-12 col-xs-12">
+            			<h1 class="compare-header__h1">Мульти сплит-системы</h1>
+            		</div>
+            		<div class="clearfix"></div>
+            		<div class="col-md-3">
+            			
+            		</div>
+            		<div class="col-md-3">
+            			<img src="img/cond-di.png" alt="Кондиционер" class="compare-content__img">
+            			<span class="compare-content__span">Сплит система ANTARKTIK MF07AC-CH</span>
+            			<p class="compare-content__p">2 200 р.</p>
+            			<div class="clearfix"></div>
+            			<div class="compare-content__button">
+            				<a href='#' class='button button_yellow compare-content__a'>заказать</a>
+            			</div>
+            			<div class="compare-charecter">
+            				<ul>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            					<li></li>
+            				</ul>
+            			</div>
+            		</div>
+            		<div class="col-md-3"></div>
+            		<div class="col-md-3"></div>-->
+            		<div class="col-md-12 col-sm-12 col-xs-12">
+            			<h1 class="compare-header__h1">Мульти сплит-системы</h1>
+            			<div class="compare-content__table">
+            				<table>
+            					<tr>
+            						<td><div class="compare-content__first"></div></td>
+            						<td><img src="img/cond-di.png" alt="Кондиционер" class="compare-content__img"></td>
+            						<td><img src="img/cond-di.png" alt="Кондиционер" class="compare-content__img"></td>
+            						<td><img src="img/cond-di.png" alt="Кондиционер" class="compare-content__img"></td>
+            					</tr>
+            					<tr>
+            						<td>Наименование</td>
+            						<td><span class="compare-content__span">Сплит система ANTARKTIK MF07AC-CH</span></td>
+            						<td><span class="compare-content__span">Сплит система ANTARKTIK MF07AC-CH</span></td>
+            						<td><span class="compare-content__span">Сплит система ANTARKTIK MF07AC-CH</span></td>
+            					</tr>
+            					<tr>
+            						<td><p class="compare-td__p">Цена</p></td>
+            						<td>
+            							<p class="compare-content__p">2 200 р.</p>
+            							<div class="clearfix"></div>
+            							<div class="compare-content__button">
+            								<a href='#' class='button button_yellow compare-content__a'>заказать</a>
+            							</div>
+            						</td>
+            						<td>
+            							<p class="compare-content__p">2 200 р.</p>
+            							<div class="clearfix"></div>
+            							<div class="compare-content__button">
+            								<a href='#' class='button button_yellow compare-content__a'>заказать</a>
+            							</div>
+            						</td>
+            						<td>
+            							<p class="compare-content__p">2 200 р.</p>
+            							<div class="clearfix"></div>
+            							<div class="compare-content__button">
+            								<a href='#' class='button button_yellow compare-content__a'>заказать</a>
+            							</div>
+            						</td>
+            					</tr>
+            					<tr>
+            						<td><p class="compare-td__p">Сборка</p></td>
+            						<td><p class="compare-td__p">КНР</p></td>
+            						<td><p class="compare-td__p">КНР</p></td>
+            						<td><p class="compare-td__p">КНР</p></td>
+            					</tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            					<tr></tr>
+            				</table>
+            			</div>
+            		</div>
+            	</div> 
+
+            </div>
+		</div>
+		<div class="footer">
+			<?php require_once('includes/footer.php'); ?>
+		</div>
+	</div>
+</body>
+</html>
